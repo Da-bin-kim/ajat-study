e879f69e1de9913abd37b4d4479cb8821223bc67
Da-bin-kim <76606378+Da-bin-kim@users.noreply.github.com>
2024-10-22 20:21:07 +0900

    [Bronze III] Title: 이진수, Time: 184 ms, Memory: 17972 KB -BaekjoonHub

--- 백준/Bronze/3460. 이진수/이진수.java
+++ 백준/Bronze/3460. 이진수/이진수.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt(); 
+        int t = in.nextInt();  // 테스트 케이스의 개수
         
         for (int i = 0; i < t; i++) {
-            String b = "";
-            int N = in.nextInt();
-            while (N != 0) {
-                b += N % 2;
-                N = N / 2;
-            }
+            int n = in.nextInt();
+            int position = 0;  // 비트 위치
             
-            for (int j = 0; j < b.length(); j++) {
-                if (b.charAt(j) == '1') {
-                    System.out.print(j + " ");
+            while (n > 0) {
+                if ((n & 1) == 1) {  // 현재 비트가 1인지 확인
+                    System.out.print(position + " ");
                 }
+                n = n >> 1;  // 오른쪽으로 1 비트 이동
+                position++;  // 비트 위치 증가
             }
-            System.out.println(); 
+            System.out.println();  // 각 테스트 케이스마다 줄바꿈
         }
+        
+        in.close();  // Scanner 닫기
     }
 }
